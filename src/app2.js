c40c9b697c423c9eb295f17f45cedd110c74bf2c
victor1338 <48907925+victor1338@users.noreply.github.com>
2022-11-19 17:08:00 +0800

    2 files modified

--- src/app2.js
+++ src/app2.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import './style.css';
 
+export default function App2() {
+  fetch(
+    'https://resource.data.one.gov.hk/td/traffic-detectors/irnAvgSpeed-all.xml'
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, 'application/xml');
+      console.log(xml);
+    })
+    .catch(console.error);
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import App2 from './app2';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -9,5 +10,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <App2 />
   </StrictMode>
 );
