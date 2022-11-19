4e80bebc3b9472e4e470f4cb424515c8717121db
victor1338 <48907925+victor1338@users.noreply.github.com>
2022-11-19 19:04:54 +0800

    3 files modified

--- src/index.js
+++ src/index.js
@@ -13,3 +13,14 @@ root.render(
     <App2 />
   </StrictMode>
 );
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
